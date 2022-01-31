74f0fcddc8dbde2e9715eb905f300c6fb4bef870
Oliver Cadman <o.cadman@live.co.uk>
2022-01-31 18:16:06 +0000

    Conditionally render register, sign in, and find a dep buttons

--- dept/settings.py
+++ dept/settings.py
@@ -95,6 +95,9 @@ DATABASES = {
     }
 }
 
+# Email confirmation to be used in development
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
