21f55b9c4d4ad63862006c413aa613eee89acc9a
Oliver Cadman <o.cadman@live.co.uk>
2022-02-01 11:31:38 +0000

    Added initial UserProfile, Instrument and UnavailableDate models, and registered them to admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserProfile, Instrument, UnavailableDate
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Instrument)
+admin.site.register(UnavailableDate)
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=60, null=True, blank=True)
+    last_name = models.CharField(max_length=60, null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    country = CountryField(blank_label="Country", null=True, blank=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+
+class Instrument(models.Model):
+
+    # Assigning first value in 'choices' tuple to avoid
+    # errors and seperate logic.
+    VOCALS = "Vocals"
+    GUITAR = "Guitar"
+    BASS_GUITAR = "Bass Guitar"
+    KEYBOARD = "Keyboard"
+    PIANO = "Piano"
+    DRUMS = "Drums"
+    PERC = "Percussion"
+    TRUMPET = "Trumpet"
+    TENOR_TROMB = "Tenor Trombone"
+    BASS_TROMB = "Bass Trombone"
+    SOUSAPHONE = "Sousaphone"
+    TENOR_SAX = "Tenor Saxophone"
+    ALTO_SAX = "Alto Saxphone"
+    BARITONE_SAX = "Baritone Saxophone"
+    FLUTE = "Flute"
+    CLARINET = "Clarinet"
+    ACCORDION = "Accordion"
+    VIOLIN = "Violin"
+    CELLO = "Cello"
+    DOUBLE_BASS = "Double Bass"
+    VIOLA = "Viola"
+    VIBRAPHONE = "Vibraphone"
+    HARMONICA = "Harmonica"
+
+    # Define the choices available to the user when selecting their instruments.
+    # Passed as 'choices' argument in CharField.
+    INSTRUMENTS = [
+        (VOCALS, "vocals"),
+        (GUITAR, "guitar"),
+        (BASS_GUITAR, "bass guitar"),
+        (KEYBOARD, "keyboard"),
+        (PIANO, "piano"),
+        (DRUMS, "drums"),
+        (PERC, "percussion"),
+        (TRUMPET, "trumpet"),
+        (TENOR_TROMB, "tenor trombone"),
+        (BASS_TROMB, "bass_trombone"),
+        (SOUSAPHONE, "sousaphone"),
+        (ALTO_SAX, "alto saxophone"),
+        (TENOR_SAX, "tenor saxophone"),
+        (BARITONE_SAX, "baritone saxophone"),
+        (FLUTE, "flute"),
+        (CLARINET, "clarinet"),
+        (ACCORDION, "accordion"),
+        (VIOLIN, "violin"),
+        (VIOLA, "viola"),
+        (CELLO, "cello"),
+        (DOUBLE_BASS, "double bass"),
+        (HARMONICA, "harmonica"),
+        (VIBRAPHONE, "vibraphone")
+    ]
+
+    instrument_name = models.CharField(max_length=50,
+                                       choices=INSTRUMENTS, default="")
+    related_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE,
+                                     related_name="related_user")
+
+    def __str__(self):
+        return self.instrument_name
+
+
+
+class UnavailableDate(models.Model):
+
+    date = models.DateTimeField(auto_now=False, null=True)
+    related_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE,
+                                     related_name="unavailable_user")
+
+    def __str__(self):
+
+        return self.date
+
+    
+
+
