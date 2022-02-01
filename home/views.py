db9bf55f0de2db6af84b1812264740b5fe2db4c0
Oliver Cadman <o.cadman@live.co.uk>
2022-02-01 15:49:20 +0000

    Redirect to profile page if referral url is from signup page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+
+from django.conf import settings
 
 
 class IndexView(View):
     def get(self, request):
+        
+        referrer_url = request.META.get("HTTP_REFERER")
+        if referrer_url is not None:
+            
+            if referrer_url == f"{settings.DOMAIN_NAME}accounts/signup/":
+                return redirect("profile", request.user)
+            else:
+                print(False)
+
+            
+
         return render(request, "home/index.html")
