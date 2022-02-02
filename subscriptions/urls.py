bc81adadd2a46bfc71ac81ce60010d90030ab270
Oliver Cadman <o.cadman@live.co.uk>
2022-02-02 18:40:12 +0000

    Added function 'create_checkout_session', routed in urls

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import create_checkout_session, SubscriptionChoiceView
+from .views import (create_checkout_session, SubscriptionChoiceView,
+                    CheckoutSuccessView, CheckoutCancelledView, get_stripe_public_key)
 
 urlpatterns = [
-    path('', create_checkout_session, name="checkout"),
-    path('choose_subscription', SubscriptionChoiceView.as_view(), name="subscription_choices")
+    path('choose_subscription', SubscriptionChoiceView.as_view(), name="subscription_choices"),
+    path('checkout', create_checkout_session, name="checkout"),
+    path("success", CheckoutSuccessView.as_view(), name="checkout_success"),
+    path("cancelled", CheckoutCancelledView.as_view(), name="checkout_cancelled")
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,12 +1,13 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.views import View
-
 from django.conf import settings
 
-import os
-if os.path.exists("env.py"):
-    import env
+from profiles.models import UserProfile
+
+import stripe
+
 
 
 class SubscriptionChoiceView(View):
@@ -15,28 +16,26 @@ class SubscriptionChoiceView(View):
         tier_one_price = settings.TIER_ONE_PRICE
         tier_two_price = settings.TIER_TWO_PRICE
 
+        tier_one_price_id = settings.STRIPE_TIERONE_PRICE_ID
+        tier_two_price_id = settings.STRIPE_TIERTWO_PRICE_ID
+        
+
         context = {
             "tier_one_price": tier_one_price,
-            "tier_two_price": tier_two_price
+            "tier_two_price": tier_two_price,
+            "tier_one_price_id": tier_one_price_id,
+            "tier_two_price_id": tier_two_price_id
         }
 
         return render(request, "subscriptions/subscription_choices.html",
                       context=context)
-
+                      
 
 @csrf_exempt
 def create_checkout_session(request):
-    if request.POST:
-        print(request.POST["priceId"])
-    
-    tier_one_price_id = os.environ.get("STRIPE_TIERONE_PRICE_ID")
-    tier_two_price_id = os.environ.get("STRIPE_TIERTWO_PRICE_ID")
+    pass
 
-    context = {
-        "tier_one_price_id": tier_one_price_id,
-        "tier_two_price_id": tier_two_price_id
-    }
-    
-    return render(request, "subscriptions/checkout.html", context=context)
+
+
 
 
