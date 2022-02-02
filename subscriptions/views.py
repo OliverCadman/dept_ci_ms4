06a018cb421f3374149c0cad78e8e5cfbffb1f44
Oliver Cadman <o.cadman@live.co.uk>
2022-02-02 22:46:06 +0000

    Contructed webhook event and added listeners

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,12 +1,14 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from django.views import View
 from django.conf import settings
 
 from profiles.models import UserProfile
 
 import stripe
+import json
 
 
 
@@ -82,9 +84,73 @@ def create_checkout_session(request):
         except stripe.error.CardError as e:
             print(e)
 
-
+@require_POST
+@csrf_exempt
 def webhook(request):
-    pass
+  payload = request.body
+  sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
+  event = None
+
+  stripe.api_key = settings.STRIPE_SECRET_KEY
+  stripe_wh_secret = settings.STRIPE_WH_SECRET
+
+  try:
+    event = stripe.Webhook.construct_event(
+        payload, sig_header, stripe_wh_secret
+    )
+  except ValueError as e:
+    # Invalid payload
+    return HttpResponse(status=400)
+
+  # Handle the event
+  if event["type"] == 'payment_intent.succeeded':
+    payment_intent = event.data.object # contains a stripe.PaymentIntent
+    print('PaymentIntent was successful!')
+  elif event["type"] == 'payment_method.attached':
+    payment_method = event.data.object # contains a stripe.PaymentMethod
+    print('PaymentMethod was attached to a Customer!')
+  elif event["type"] == 'invoice.upcoming':
+    print("Invoice is upcoming")
+  elif event["type"] == 'payment_intent.requires_action':
+    print('You broke twat')
+  else:
+    print('Unhandled event type {}'.format(event.type))
+
+  return HttpResponse(status=200)
+
+
+# @csrf_exempt
+# def webhook(request):
+#     """
+#     Listens for webhooks from Stripe
+#     """
+
+#     stripe_api_key = settings.STRIPE_SECRET_KEY
+#     stripe_wh_secret = settings.STRIPE_WH_SECRET
+
+#     event = None
+#     payload = request.data
+#     sig_header = request.headers['STRIPE_SIGNATURE']
+
+#     try:
+#         event = stripe.Webhook.construct_event(
+#             payload, sig_header, stripe_wh_secret
+#         )
+#         print(event)
+#     except ValueError as e:
+#         # Invalid payload
+#         return HttpResponse(e, status=400)
+#     except stripe.error.SignatureVerificationError as e:
+#         # Invalid signature
+#         return HttpResponse(e, status=400)
+#     except Exception as e:
+#         return HttpResponse(e, status=400)
+
+#     # Handle the event
+#     print('Unhandled event type {}'.format(event['type']))
+
+#     print('Success')
+#     return HttpResponse(status=200)
 
         
 class CheckoutSuccessView(View):
