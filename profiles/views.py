9ff278406e600316b8df8677692e0df7a60008fb
Oliver Cadman <o.cadman@live.co.uk>
2022-02-08 15:27:28 +0000

    Add success message when profile is saved

--- profiles/views.py
+++ profiles/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 from .models import UserProfile
-
-from .forms import UserProfileForm, EquipmentForm
+from .forms import UserProfileForm, EquipmentForm, AudioForm
+from .validators import validate_audiofile
 
 
 
@@ -18,7 +18,7 @@ class ProfileView(View):
     def get(self, request, user_name):
         print("Request:", request.META.get("HTTP_REFERER"))
         return render(request, "profiles/profile.html")
-        
+
 
 def edit_profile(request, username):
 
@@ -55,20 +55,24 @@ def edit_profile(request, username):
                         post = equipment_form.save(commit=False)
                         post.related_user = user_profile
                         post.save()
+                        messages.success(request, "Profile successfully updated")
                     except Exception as e:
                         print(f"Exception with Equipment Form: {e}")
 
             equipment_form = EquipmentForm(request.POST, instance=user_profile)
-
-        
+            audio_form = AudioForm(request.POST, instance=user_profile)
+   
     else:
+        audio_form = AudioForm()
         user_profile_form = UserProfileForm()
         equipment_form = EquipmentForm()
-  
+
+    validate_audiofile(filename="file.wav")
 
     context = {
         "user_profile_form": user_profile_form,
         "equipment_form": equipment_form,
+        "audio_form": audio_form,
         "page_name": "user_profile_form",
         "user_name": current_user.username
     }
