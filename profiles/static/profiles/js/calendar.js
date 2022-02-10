a0743501ddcf17a008107067f1e2fd211ecdc402
Oliver Cadman <o.cadman@live.co.uk>
2022-02-10 21:44:13 +0000

    Grab array of clicked, sorted  dates and post to backend

---
+++ profiles/static/profiles/js/calendar.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+    const dateArray = []
+    let clickCount = 0;
+    const calendarElement = document.getElementById("calendar_wrapper")
+    const calendar = new FullCalendar.Calendar(calendarElement, {
+        initialView: "dayGridMonth",
+        dateClick: function(e) {
+            clickCount++
+            if (!e.dayEl.classList.contains("selected")) {
+                e.dayEl.classList.add("selected")
+                console.log(e.dayEl)
+                dateArray.push(e.dateStr);
+                  dateArray.sort()
+
+            } else  {
+                e.dayEl.classList.remove("selected")
+                let dateToRemove = e.dateStr
+                let dateIndex = dateArray.indexOf(dateToRemove)
+                dateArray.splice(dateIndex, 1)
+                dateArray.sort()     
+            }
+
+            collectDateArrayAndSubmit(dateArray)
+        }
+
+    });
+    calendar.render();
+
+    let isSubmitting = false;
+
+    function collectDateArrayAndSubmit(array){
+        const csrfToken = $("input[name='csrfmiddlewaretoken']")[2].value
+        const dateArray = array
+        const formData = new FormData();
+        console.log(dateArray)
+        const username = $("#user_name_3").val()
+        
+        const submitBtn = $("#submit_unavailability");
+        submitBtn.on("click", function() {
+            if (isSubmitting) {
+                return; 
+            }
+            isSubmitting = true;
+            console.log('click')
+            const url = `/profile/upload_unavailability/${username}`;
+            $.post({
+            url: url,
+            headers: {
+                "X-CSRFToken": csrfToken,
+            },
+            data: {
+                "date_array": dateArray
+            },
+            success: function (res) {
+                isSubmitting = false;
+                console.log(res.status);
+            },
+            }); 
+            return false;
+        })
+    }
+})
