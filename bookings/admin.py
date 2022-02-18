d4bad256a7c548991ecbd5e5b2771d9d2d1ce7ef
Oliver Cadman <o.cadman@live.co.uk>
2022-02-18 14:55:47 +0000

    Created admin class for Invitation model

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Invitation
+
+class InvitationAdmin(admin.ModelAdmin):
+
+    list_display = ("invitation_number", "invite_sender",
+                    "invite_receiver")
 
 # Register your models here.
+admin.site.register(Invitation, InvitationAdmin)
+
