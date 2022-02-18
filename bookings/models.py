48481229f9df9e39d8ad500a6b9fb8c12e727283
Oliver Cadman <o.cadman@live.co.uk>
2022-02-18 14:35:46 +0000

    Created model 'Invitation'

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django_countries.fields import CountryField
+from profiles.models import UserProfile
+import uuid
 
-# Create your models here.
+
+class Invitation(models.Model):
+    invitation_number = models.CharField(max_length=50, null=False, editable=False)
+    invite_sender = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                      related_name="invitations_sent", null=True)
+    invite_receiver = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                        related_name="invitations_received", null=True)
+    event_name = models.CharField(max_length=150, null=True)
+    event_city = models.CharField(max_length=100)
+    event_country = CountryField()
+    event_datetime = models.DateTimeField()
+    date_of_invitation = models.DateField(auto_now_add=True)
+    additional_info = models.TextField(null=True, blank=True)
+
+    def generate_invitation_number(self):
+        """
+        Generate random UUID string to represent invitation
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        if not self.invitation_number:
+            self.invitation_number = self.generate_invitation_number
+            super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.invite_sender}'s invite for {self.invite_receiver}"
