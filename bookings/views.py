50163a228df48964bbe27d9ff792d7a1a6fd6853
Oliver Cadman <o.cadman@live.co.uk>
2022-02-19 16:31:25 +0000

    Create function 'invitation_form_view'

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponse
+from django.contrib import messages
+
+from .forms import InvitationForm
 
 # Create your views here.
+
+def invitation_form_view(request):
+    if request.POST:
+        
+        messages.success(request, "Invitation Sent")
+        return redirect(reverse("profile", kwargs={"user_name": request.user}))
+    
