ea316b36ef65984aa7d6fdc32af71cffcae8ef77
Oliver Cadman <o.cadman@live.co.uk>
2022-02-19 14:03:19 +0000

    Wrapped booking form in crispy form helper

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from django_countries import Countries
+from django.utils.safestring import mark_safe
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Div, Submit, Fieldset, HTML
+from crispy_forms.bootstrap import PrependedText
+
+from bootstrap_datepicker_plus.widgets import DateTimePickerInput
+
 from .models import Invitation
 from profiles.models import UserProfile
+
 
 
 class InvitationForm(forms.ModelForm):
@@ -11,30 +19,69 @@ class InvitationForm(forms.ModelForm):
         exclude = ("invite_sender", "invite_receiver",
                    "is_accepted", "date_of_invitation")
 
+
+    def __init__(self, *args, **kwargs):
+        super(InvitationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_action = "invite"
+        self.helper.form_method = "post"
+        self.helper.form_id = "invitation_form"
+        self.helper.add_input(Submit('submit', "Submit"))
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    HTML("<h2>Invite {{user.first_name}} to your gig.</h2>"),
+                    "event_name",
+                    "artist_name",
+                    "event_city",
+                    "event_country",
+                    "event_datetime",
+                    "fee",
+                    "additional_info"   
+                )
+            )
+        )
+        self.helper["fee"].wrap(PrependedText, mark_safe("<i class='fas fa-pound-sign'></i>"))
+
+
     event_name = forms.CharField(
-        widget=forms.TextInput(attrs={
-            "placeholder": "Please enter the name of your event"
-        })
+        label="The name of your event",
+        widget=forms.TextInput()
+    )
+
+    artist_name = forms.CharField(
+        label="The name of the artist/band",
+        widget=forms.TextInput()
     )
 
     event_city = forms.CharField(
-        widget=forms.TextInput(attrs={
-               "placeholder": "The town/city where the event is taking place"
-        })
+        label="The city where the event is taking place",
+        widget=forms.TextInput()
     )
 
     event_country = forms.ChoiceField(choices=Countries)
 
     event_datetime = forms.DateTimeField(
-        widget=forms.SplitDateTimeWidget()
+        label="Date and Time of your Event",
+        widget=DateTimePickerInput(format="%d/%m/%Y %H:%M")
+    )
+
+    fee = forms.DecimalField(
+        widget=forms.NumberInput()
     )
 
     additional_info = forms.CharField(
+        label="Any additional info?",
+        required=False,
         widget=forms.Textarea(attrs={
             "rows": "3",
-            "placeholder": "Additional Information about your event"
         })
     )
+
+    
+    
+
+    
 
 
     
