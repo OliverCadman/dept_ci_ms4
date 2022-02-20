fe058edffa75bb0c31f766bb8a5a7fa95cd84114
Oliver Cadman <o.cadman@live.co.uk>
2022-02-20 22:09:07 +0000

    Adjusted url path

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import invitation_form_view
 
 urlpatterns = [
-    path('invitation', invitation_form_view, name="invitation"),
+    path('invite', invitation_form_view, name="invitation"),
 ]
