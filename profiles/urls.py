4efbd53af9fb784d61b045c4d2a3bc57a47c48dc
Oliver Cadman <o.cadman@live.co.uk>
2022-02-22 13:45:15 +0000

    Hooked Dashboard view up to urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import (ProfileView, edit_profile, upload_audio,
                     upload_unavailable_dates, get_users_unavailable_dates,
-                    get_users_tracks)
+                    get_users_tracks, DashboardView)
 
 urlpatterns = [
+    path('edit_profile', edit_profile, name="edit_profile"),
+    path("dashboard", DashboardView, name="dashboard"),
     path('<str:user_name>', ProfileView.as_view(), name="profile"),
-    path('edit_profile/<str:username>', edit_profile, name="edit_profile"),
     path("upload_audio/<str:username>", upload_audio, name="upload_audio"),
-    path("upload_unavailability/<str:username>", upload_unavailable_dates, name="upload_unavailability"),
     path("get_users_unavailable_dates/<str:username>", get_users_unavailable_dates),
-    path("get_users_tracks/<str:username>", get_users_tracks)
+    path("upload_unavailability/<int:user_id>", upload_unavailable_dates, name="upload_unavailability"),
+    path("get_users_tracks/<int:user_id>", get_users_tracks)
 ]
