3ad5313384befd6964790df0d1ab90ef59191289
Oliver Cadman <o.cadman@live.co.uk>
2022-02-26 13:50:03 +0000

    Added modal to message invite sender/receiver, passing in fname and lname as data

--- profiles/static/profiles/js/invite_detail_modal.js
+++ profiles/static/profiles/js/invite_detail_modal.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    $(".invitation_card_btn").click(function () {
+    $(".view_detail_btn").click(function (e) {
+      e.stopPropagation();
       console.log("hello");
       $("#modal_event_name").html($(this).data("event-name"));
       $("#modal_invite_personage").html(
@@ -9,6 +10,15 @@ $(document).ready(function() {
         `${$(this).data("event-city")}, ${$(this).data("event-country")}`
       );
       $("#modal_event_datetime").html($(this).data("event-datetime"));
-      $("#modal_event_additional").html($(this).data("additional-info"));
+
+      if ($(this).data("additional-info") !== undefined) {
+        $("#modal_additional").html($(this).data("additional-info"));
+      } else {
+        $("#modal_additional").html("No Additional Information")
+      }
     });
+
+    $(".message_modal_btn").click(function() {
+      $("#message_modal_header").html(`${$(this).data("invite-fname")} ${$(this).data("invite-lname")}`)
+    })
 })
