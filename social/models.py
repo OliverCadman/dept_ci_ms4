76cb17836dc909b3dd06bc51f9d9ce3721bd2326
Oliver Cadman <o.cadman@live.co.uk>
2022-02-26 16:38:22 +0000

    Create new model 'Message' and register with admin

--- social/models.py
+++ social/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 
+from profiles.models import UserProfile
+from bookings.models import Invitation
 
+
+class Message(models.Model):
+
+    message_sender = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="sent_messages")
+    message_receiver = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="received_messages")
+    invitation_id = models.ForeignKey(Invitation, on_delete=models.CASCADE, null=True, related_name="invitation_messages")
+    date_of_message = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(max_length=400)
+
+    def __str__(self):
+        return f"Message from {self.message_sender} to {self.message_receiver}"
 
