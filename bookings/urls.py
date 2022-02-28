8ca4a53c06e21b1e80cebb80cc1fca2b0bce752f
Oliver Cadman <o.cadman@live.co.uk>
2022-02-28 20:54:01 +0000

    Removed invitation detail view from urls file

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
 from bookings.models import Invitation
-from .views import invitation_form_view, InvitationDetailView
+from .views import invitation_form_view, get_invitation_messages
 
 urlpatterns = [
     path('invite', invitation_form_view, name="invitation"),
-    path("invite/<int:pk>", InvitationDetailView.as_view(), name="invitation_detail")
+    path("get_invitation_messages/<int:pk>", get_invitation_messages, name="get_invitation_messages"),
 ]
