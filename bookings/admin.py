df4632e56a9fa4c300b76931f1c0c2536fd41d58
Oliver Cadman <o.cadman@live.co.uk>
2022-03-04 17:24:11 +0000

    Add Booking model to Admin with AudioFile inline

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import Invitation
+from .models import Invitation, Booking
+
+from profiles.models import AudioFile
+
+class AudioFileInline(admin.TabularInline):
+
+    model = AudioFile
+
+    exclude = ("related_user",)
+
+
+class BookingInline(admin.StackedInline):
+
+    model = Booking
+
 
 class InvitationAdmin(admin.ModelAdmin):
 
@@ -8,6 +22,13 @@ class InvitationAdmin(admin.ModelAdmin):
 
     readonly_fields = ("pk",)
 
+    inlines = [BookingInline]
+
+
+class BookingAdmin(admin.ModelAdmin):
+    inlines = [AudioFileInline]
+
 # Register your models here.
 admin.site.register(Invitation, InvitationAdmin)
+admin.site.register(Booking, BookingAdmin)
 
