e4c9c9da4ba756ad370f0a8fc17c78c12547816e
Oliver Cadman <o.cadman@live.co.uk>
2022-03-05 18:14:11 +0000

    Create utils.py file with render_to_pdf function

---
+++ bookings/utils.py
@@ -0,0 +1,29 @@
+from io import BytesIO
+from multiprocessing import context
+from django.http import HttpResponse
+from django.template.loader import get_template
+
+from xhtml2pdf import pisa
+
+def render_to_pdf(template_source, context_dict = {}):
+    """
+    Renders Booking Detail HTML Template to PDF Format
+
+    Arguments:
+
+    Template Source - "booking_detail_display_pdf.html"
+    Context Dict - Booking object (alias "event")
+
+    https://www.codingforentrepreneurs.com/blog/html-template-to-pdf-in-django/
+
+    """
+
+    template = get_template(template_source)
+    html = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type="application/pdf")
+    else:
+        return None
