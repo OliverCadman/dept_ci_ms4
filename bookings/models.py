3dd844b94a09b39dc70643bc04836911648a10e7
Oliver Cadman <o.cadman@live.co.uk>
2022-03-05 17:24:11 +0000

    Added address fields to booking model

--- bookings/models.py
+++ bookings/models.py
@@ -46,6 +46,11 @@ class Invitation(models.Model):
 class Booking(models.Model):
     related_invitation = models.OneToOneField(Invitation, on_delete=models.CASCADE,
                                               related_name="related_booking")
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    country = CountryField(null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
     travel_provided = models.BooleanField(default=False, null=True)
     travel_info = models.TextField(null=True, blank=True)
     backline_provided = models.BooleanField(default=False, null=True)
