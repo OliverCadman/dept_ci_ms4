3e31a353f2f8cdad60b92d4eda2e432280e8df15
Oliver Cadman <o.cadman@live.co.uk>
2022-03-06 16:28:17 +0000

    Add Notification model to admin.py

--- social/admin.py
+++ social/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Message
+from .models import Message, Notification
 
 class MessageAdmin(admin.ModelAdmin):
     list_display = ("__str__", "date_of_message",)
 
+
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Notification)
