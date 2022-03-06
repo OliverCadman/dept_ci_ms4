51b9dffb6f9850f31c7b3cd9f7af45aa24a79abe
Oliver Cadman <o.cadman@live.co.uk>
2022-03-06 15:53:42 +0000

    added MessageAdmin to social DB

--- social/admin.py
+++ social/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Message
 
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "date_of_message",)
 
-admin.site.register(Message)
+admin.site.register(Message, MessageAdmin)
