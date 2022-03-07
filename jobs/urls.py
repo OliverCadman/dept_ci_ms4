5d013e9c7939588bf5a2848993a238c1b5b55429
Oliver Cadman <o.cadman@live.co.uk>
2022-03-07 16:17:53 +0000

    Created View for job landing page, and routed

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import JobPageView
+from .views import JobPageView, FindADepLandingPageView
 
 urlpatterns = [
-    path('<str:job_name>', JobPageView.as_view(), name="job_page")
+    path('find_a_dep', FindADepLandingPageView.as_view(), name="find_a_dep_landing_page" ),
+    path('<str:job_name>', JobPageView.as_view(), name="job_page"),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 
 class JobPageView(View):
@@ -7,6 +8,16 @@ class JobPageView(View):
     def get(self, request, job_name):
         print(job_name)
         return render(request, "jobs/job_list.html")
+
+class FindADepLandingPageView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        context = {
+        "page_name": "find_a_dep_landing_page"
+        }
+    
+        return render(request, "jobs/findadep_landing_page.html", context=context)
 
 
 
