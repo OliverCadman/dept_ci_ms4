3a58a33b61c77a7f3d8d9336c35fd433544bff78
Oliver Cadman <o.cadman@live.co.uk>
2022-03-07 10:01:02 +0000

    Added post_save signal to create notification on invite sent

--- social/signals.py
+++ social/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import receiver
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_save
 from django.shortcuts import get_object_or_404
 
 from bookings.models import Invitation
@@ -20,3 +20,17 @@ def send_notification_on_decline(sender, instance, **kwargs):
         notification_type=3,
         declined_invitation=declined_invitation
     )
+
+@receiver(post_save, sender=Invitation)
+def send_notification_on_invite_sent(sender, instance, **kwargs):
+    invitation = instance
+
+    invite_sender = invitation.invite_receiver
+
+    Notification.objects.create(
+        notification_sender=invite_sender,
+        notification_receiver=invitation.invite_receiver,
+        notification_type=1,
+        related_invitation=invitation
+    )
+
