92574ffcbd8d271b92d830d201249f579b987d94
Oliver Cadman <o.cadman@live.co.uk>
2022-03-08 15:22:55 +0000

    Added user profile objects to dep list page

--- jobs/views.py
+++ jobs/views.py
@@ -17,7 +17,7 @@ class FindADepLandingPageView(ListView):
 
     model = UserProfile
 
-    context_object_name = "users"
+    context_object_name = "dep_collection"
 
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
