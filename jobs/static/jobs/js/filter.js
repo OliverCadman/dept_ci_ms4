67b9c5be2969cdd2c2a9b0ed0184a4bd38228130
Oliver Cadman <o.cadman@live.co.uk>
2022-03-09 20:18:28 +0000

    Check for unused fields in filter form and disable fields if not used

--- jobs/static/jobs/js/filter.js
+++ jobs/static/jobs/js/filter.js
@@ -1,28 +1,46 @@
 $(document).ready(function() {
-    /*
-    JavaScript code to allow user to execute search
-    immediately upon selecting a search criteria in the
-    form select dropdown list.
+  /*
+    JavaScript code prevents all fields being added as query params
+    to the URL, therefore allowing for filtering/searching on a one-by-one
+    basis.
+
+    Disables each field if they fulfill condition (unchecked checkbox
+    disables "available_today" field, empty "city" searchbox value disables
+    field, etc.)
+
     */
 
-    // Grab select element and attach 'onchange' event listener.
-    const selectEl = $("#instrument_filter")
-    selectEl.on("change", function() {
-        let parentFormId = "#instrument_filter_form"
-        FilterOnSelectChange(parentFormId)
-    })
 
+    // Modify the value of the "Available Today" checkbox to "true" if checked.
+    $("#available_today_checkbox").change(function () {
+        if (this.checked) {
+        $(this).val("true");
+        }
+    });
 
-    function FilterOnSelectChange(formId) {
-        /* Submits the form immediately upon change of select value
-        
-        Params: 
+    // Allows for the checkbox to remain checked upon page refresh, if checked
+    // by the user to search for a member who is "Available Today".
+    if ($("#available_today_checkbox").val() == "true") {
+        $("#available_today_checkbox").prop("checked", true);
+    }
 
-            formId - If of the parent form to submit.
-        */
-        let form = $(formId)
-        if (typeof(form) != undefined || typeof(form) != null) {
-            form.submit();
+    // Checks the values of all fields in filter form.
+    // Disables the unused fields upon submission of form.
+    function checkForUnusedFilterFields() {
+        if ($("#city").val() == "") {
+        $("#city").prop("disabled", true);
+        }
+
+        if ($("#instrument_filter").val() == "all") {
+        $("#instrument_filter").prop("disabled", true);
+        }
+
+        if ($("#available_today_checkbox").val() == "false") {
+        $("#available_today_checkbox").prop("disabled", true);
+        } else {
+        $("#available_today_checkbox").checked;
         }
     }
+
+  $("#instrument_filter_form").on("submit", checkForUnusedFilterFields);
 })
