67421b1f24b845df12ca44a6b123d16b0a88bc90
Oliver Cadman <o.cadman@live.co.uk>
2022-03-11 11:21:21 +0000

    Refactored code to accommodate for usage in dep_list.html

--- profiles/static/profiles/js/display_star_rating.js
+++ profiles/static/profiles/js/display_star_rating.js
@@ -14,7 +14,8 @@ $(document).ready(function() {
        let rating = parseInt(hiddenRatingInputs[i].value);
 
        // Traverse DOM to find the star_rating_wrapper relative to each input in the loop.
-       let starRatingWrapper = hiddenRatingInputs[i].parentElement.children[2].children[0]
+       let starRatingWrapper = hiddenRatingInputs[i].parentElement.children[1]
+       console.log(starRatingWrapper)
         
        // Create filled FontAwesome stars for each point of rating
        for (let k = 0; k < rating; k++) {
@@ -41,40 +42,50 @@ $(document).ready(function() {
     (out of 5) as outlined stars.
     */ 
 
-
+    
     // Grab values from json_script
-    let averageRating = document.getElementById('average_rating').textContent;
-    const starIconContainers = document.getElementsByClassName("header_star_icons");
+    const currentPath = window.location.href
+    const endPoint = currentPath.split("/")[3]
+    console.log(endPoint)
+    if (endPoint === "profile") {
+         let averageRating =
+           document.getElementById("average_rating").textContent;
+         const starIconContainers =
+           document.getElementsByClassName("header_star_icons");
 
-    // Loop through both containers (one for mobile, one for larger screens)
-    for (container of starIconContainers) {
-        let maxRating = 5;
-        averageRating = parseInt(averageRating);
-        let rateDelta = maxRating - averageRating
+         // Loop through both containers (one for mobile, one for larger screens)
+         for (container of starIconContainers) {
+           let maxRating = 5;
+           averageRating = parseInt(averageRating);
+           let rateDelta = maxRating - averageRating;
 
-        // Create filled FontAwesome star icons from average rating
-        for (let i = 0; i < averageRating; i++) {
-            let filledStarIcon = document.createElement("i");
-            filledStarIcon.className = "rating_star fas fa-star"
-            container.appendChild(filledStarIcon);
+           // Create filled FontAwesome star icons from average rating
+           for (let i = 0; i < averageRating; i++) {
+             let filledStarIcon = document.createElement("i");
+             filledStarIcon.className = "rating_star fas fa-star";
+             container.appendChild(filledStarIcon);
+           }
 
-        }
+           // Create FontAwesome outlined stars for remainder
+           for (let j = 0; j < rateDelta; j++) {
+             let outlinedStarIcon = document.createElement("i");
+             outlinedStarIcon.className = "rating_star far fa-star";
+             container.appendChild(outlinedStarIcon);
+             container.appendChild;
+           }
 
-        // Create FontAwesome outlined stars for remainder
-        for (let j = 0; j < rateDelta; j++) {
-            let outlinedStarIcon = document.createElement("i");
-            outlinedStarIcon.className = "rating_star far fa-star";
-            container.appendChild(outlinedStarIcon);
-            container.appendChild
-        }
+           // Get the number of reviews from json_script.
+           // Display the number of reviews next to the average star rating.
+           let numOfReviews =
+             document.getElementById("num_of_reviews").textContent;
+           let numOfReviewsDisplay = document.createElement("span");
+           numOfReviewsDisplay.textContent = `(${numOfReviews})`;
+           numOfReviewsDisplay.className = "secondary_font white_font ms-1";
+           container.appendChild(numOfReviewsDisplay);
+         }
+    } else {
         
-        // Get the number of reviews from json_script.
-        // Display the number of reviews next to the average star rating.
-        let numOfReviews = document.getElementById("num_of_reviews").textContent;
-        let numOfReviewsDisplay = document.createElement("span")
-        numOfReviewsDisplay.textContent = `(${numOfReviews})`;
-        numOfReviewsDisplay.className = "secondary_font white_font ms-1"
-        container.appendChild(numOfReviewsDisplay);
     }
+   
 
 })
