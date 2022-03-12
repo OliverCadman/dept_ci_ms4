13ec451359baff2c3eeb2f91a1a287a2e71c742f
Oliver Cadman <o.cadman@live.co.uk>
2022-03-12 15:50:26 +0000

    Added url to post_job view

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import JobPageView, FindADepLandingPageView
+from .views import DepListView, JobListView, post_job
 
 urlpatterns = [
-    path('find_a_dep', FindADepLandingPageView.as_view(), name="find_a_dep_landing_page" ),
-    path('<str:job_name>', JobPageView.as_view(), name="job_page"),
+    path('find_a_dep', DepListView.as_view(), name="dep_list" ),
+    path('find_a_job', JobListView.as_view(), name="job_list"),
+    path("post_job", post_job, name="post_job")
 ]
