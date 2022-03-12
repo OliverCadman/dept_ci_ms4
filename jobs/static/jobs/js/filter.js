cde87f002194d07a5c36d468c1cf7604d0d1e093
Oliver Cadman <o.cadman@live.co.uk>
2022-03-12 15:47:39 +0000

    Disabled genre dropdown value if 'All' selected

--- jobs/static/jobs/js/filter.js
+++ jobs/static/jobs/js/filter.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   /*
     JavaScript code prevents all fields being added as query params
     to the URL, therefore allowing for filtering/searching on a one-by-one
@@ -10,37 +10,40 @@ $(document).ready(function() {
 
     */
 
+  // Modify the value of the "Available Today" checkbox to "true" if checked.
+  $("#available_today_checkbox").change(function () {
+    if (this.checked) {
+      $(this).val("true");
+    }
+  });
 
-    // Modify the value of the "Available Today" checkbox to "true" if checked.
-    $("#available_today_checkbox").change(function () {
-        if (this.checked) {
-        $(this).val("true");
-        }
-    });
+  // Allows for the checkbox to remain checked upon page refresh, if checked
+  // by the user to search for a member who is "Available Today".
+  if ($("#available_today_checkbox").val() == "true") {
+    $("#available_today_checkbox").prop("checked", true);
+  }
 
-    // Allows for the checkbox to remain checked upon page refresh, if checked
-    // by the user to search for a member who is "Available Today".
-    if ($("#available_today_checkbox").val() == "true") {
-        $("#available_today_checkbox").prop("checked", true);
+  // Checks the values of all fields in filter form.
+  // Disables the unused fields upon submission of form.
+  function checkForUnusedFilterFields() {
+    if ($("#city").val() == "") {
+      $("#city").prop("disabled", true);
     }
 
-    // Checks the values of all fields in filter form.
-    // Disables the unused fields upon submission of form.
-    function checkForUnusedFilterFields() {
-        if ($("#city").val() == "") {
-        $("#city").prop("disabled", true);
-        }
-
-        if ($("#instrument_filter").val() == "all") {
-        $("#instrument_filter").prop("disabled", true);
-        }
-
-        if ($("#available_today_checkbox").val() == "false") {
-        $("#available_today_checkbox").prop("disabled", true);
-        } else {
-        $("#available_today_checkbox").checked;
-        }
+    if ($("#instrument_filter").val() == "all") {
+      $("#instrument_filter").prop("disabled", true);
     }
+
+    if ($("#available_today_checkbox").val() == "false") {
+      $("#available_today_checkbox").prop("disabled", true);
+    } else {
+      $("#available_today_checkbox").checked;
+    }
+
+    if($("#genre").val() == "all"){
+        $("#genre").prop("disabled", true)
+    } 
+  }
 
   $("#instrument_filter_form").on("submit", checkForUnusedFilterFields);
-})
+});
