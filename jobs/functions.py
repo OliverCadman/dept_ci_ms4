e360ef47223453361b27b8c6dc9494065c90e603
Oliver Cadman <o.cadman@live.co.uk>
2022-03-12 15:45:10 +0000

    Removed print statements

--- jobs/functions.py
+++ jobs/functions.py
@@ -5,7 +5,7 @@ def handle_deplist_get(params):
     Handles GET parameters in 'Find a Dep' page,
 
     Takes request.GET and kwargs as argument,
-    and prepares and returns a context with the 
+    and prepares and returns a context with the
     values provided in keyword arguments.
     """
 
@@ -29,24 +29,18 @@ def handle_deplist_get(params):
         context["search_params"]["last_name__iexact"] = lname_arg
 
     if "available_today" in params:
-        print("yes")
         context["available_today"] = datetime.datetime.today()
         context["availability_params"] = context["available_today"]
-    
+
     if "city" in params:
         city_arg = params["city"]
         context["city"] = city_arg
         context["search_params"]["city__iexact"] = city_arg
 
-    
+
     if "genre" in params:
         genre_arg = params["genre"]
         context["genre"] = genre_arg
         context["search_params"]["genres__genre_name"] = genre_arg
         
-
-
-        
-    
     return context
-                                
