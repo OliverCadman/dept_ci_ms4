e7591f7d575f2eaaf7734947e5896ef0a96f0f26
Oliver Cadman <o.cadman@live.co.uk>
2022-03-13 20:15:03 +0000

    Added pagination to JobView

--- jobs/views.py
+++ jobs/views.py
@@ -104,6 +104,7 @@ class JobListView(ListView):
     template_name = "jobs/job_list.html"
     model = Job
     context_object_name = "job_collection"
+    paginate_by = 6
 
     def get_context_data(self, **kwargs):
 
