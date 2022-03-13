02f2efe0bb5720b581bd55cea68f66ddc2e58bfb
Oliver Cadman <o.cadman@live.co.uk>
2022-03-13 11:21:03 +0000

    Populate job detail modal with data passed by data attribute of button

---
+++ jobs/static/jobs/js/job_detail_modal.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    /* Populates Job Detail Modals with data from Job Detail Cards
+    
+    Content is passed through the data attributes of "View Details" button
+    and appended as html to the elements with relevant ID attributes.
+    */
+    $(".job_detail_btn").click(function() {
+        $("#job_title").html($(this).data("job-title"));
+        $("#event_name").html($(this).data("event-name"));
+        $("#artist_name").html($(this).data("artist-name"));
+        $("#event_city").html($(this).data("event-city"));
+        $("#event_country").html($(this).data("event-country"));
+        $("#fee").html(`£${$(this).data("job-fee")}`);
+        $("#job_poster").html($(this).data("job-poster"));
+        $("#job_description").html($(this).data("job-description"));
+        $("#job_poster").html(`Posted by ${$(this).data("job-poster")}`);
+
+        if ($(this).data("job-poster-image")) {
+            $("#poster_profile_img_wrapper").html(
+              `<img src="${$(this).data("job-poster-image")}"
+              alt="Image of Job Advertiser"
+              width="75"
+              height="75"
+              class="modal_profile_image_thumbnail">`
+            );
+        } else {
+            $("#poster_profile_img_wrapper").html(
+              `<img src="/media/dept-logo.webp"
+              alt="Image of Job Advertiser"
+              width="75"
+              height="75"
+              class="modal_profile_image_thumbnail">`
+            );
+        }
+        
+    })
+})
