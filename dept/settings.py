4e9a203dd75a90259f8fb57f01c1a9369264eed9
Oliver Cadman <o.cadman@live.co.uk>
2022-03-15 22:48:01 +0000

    Added trailing slash to media and static url

--- dept/settings.py
+++ dept/settings.py
@@ -73,7 +73,6 @@ INSTALLED_APPS = [
 
     # S3Boto3Storage
     'storages',
-
 ]
 
 
@@ -263,10 +262,10 @@ else:
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
