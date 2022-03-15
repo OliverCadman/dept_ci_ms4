b5b12b3bd5a0a80f9ccf438c09d69544daf5ec1a
Oliver Cadman <o.cadman@live.co.uk>
2022-03-15 16:02:32 +0000

    Added pk to readonly field of JobAdmin

--- jobs/admin.py
+++ jobs/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Job
 
+class JobAdmin(admin.ModelAdmin):
+
+    readonly_fields = ("pk",)
+
 # Register your models here.
-admin.site.register(Job)
+admin.site.register(Job, JobAdmin)
