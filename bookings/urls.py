6db0848ef5ef8b91536a0ae981fe83bb87fd7ecf
Oliver Cadman <o.cadman@live.co.uk>
2022-03-19 15:25:11 +0000

    Added urls to edit and delete invitation views

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,10 +4,13 @@ from bookings.models import Invitation
 from .views import (invitation_form_view, get_invitation_messages,
                     accept_invitation, decline_invitation, booking_form, BookingSuccessView,
                     BookingDetailView, GeneratePDFFile, download_audiofile,
-                    get_invitation_id, tier_two_booking_form)
+                    get_invitation_id, tier_two_booking_form, EditInvitationForm, delete_invitation,
+                    get_review, edit_review)
 
 urlpatterns = [
     path('invite', invitation_form_view, name="invitation"),
+    path("edit_invitation/<int:invitation_id>", EditInvitationForm.as_view(), name="edit_invitation"),
+    path("delete_invitation/<int:invitation_id>", delete_invitation, name="delete_invitation"), 
     path("get_invitation_messages/<int:pk>", get_invitation_messages, name="get_invitation_messages"),
     path("accept_invitation/<int:invitation_pk>", accept_invitation, name="accept_invitation"),
     path("decline_invitation/<int:invitation_pk>", decline_invitation, name="decline_invitation"),
@@ -17,5 +20,5 @@ urlpatterns = [
     path("booking_detail/<int:pk>", BookingDetailView.as_view(), name="booking_detail"),
     path("generate_pdf/<int:booking_id>", GeneratePDFFile.as_view(), name="generate_pdf"),
     path("download_audiofile/<int:file_id>", download_audiofile, name="download_audiofile"),
-    path("get_invitation_id/<int:booking_id>", get_invitation_id),
+    path("get_invitation_id/<int:booking_id>", get_invitation_id)
 ]
