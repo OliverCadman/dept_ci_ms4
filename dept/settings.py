a931040f79bdb5b1ec905aff7330ee88f04f3e10
Oliver Cadman <o.cadman@live.co.uk>
2022-03-20 19:05:14 +0000

    Added AWS_DOWNLOAD_EXPIRE variable to settings

--- dept/settings.py
+++ dept/settings.py
@@ -158,26 +158,26 @@ else:
     }
 }
 
-if "USE_AWS" in os.environ:
-    # Cache Control
 
-    # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = "dept-bucket"
-    AWS_S3_REGION_NAME = "eu-west-2"
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-    # Static and Media Files
-    STATICFILES_STORAGE = "custom_storages.StaticStorage"
-    STATICFILES_LOCATION = "static"
+# Bucket Config
+AWS_STORAGE_BUCKET_NAME = "dept-bucket"
+AWS_S3_REGION_NAME = "eu-west-2"
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_DOWNLOAD_EXPIRE = 5000
 
-    MEDIAFILES_STORAGE = "custom_storages.MediaStorage"
-    MEDIAFILES_LOCATION = "media"
+# Static and Media Files
+STATICFILES_STORAGE = "custom_storages.StaticStorage"
+STATICFILES_LOCATION = "static"
 
-    # Override Static and Media File URLS in production
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
+MEDIAFILES_STORAGE = "custom_storages.MediaStorage"
+MEDIAFILES_LOCATION = "media"
+
+# Override Static and Media File URLS in production
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
@@ -185,8 +185,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-if "DEVELOPMENT" not in os.environ:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
