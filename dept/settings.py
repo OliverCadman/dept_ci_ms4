a18a9a83eb556ba53471061980aaaed339b241ca
Oliver Cadman <o.cadman@live.co.uk>
2022-03-20 20:57:54 +0000

    Added S3 signature version to settings as environment variable

--- dept/settings.py
+++ dept/settings.py
@@ -158,24 +158,29 @@ else:
     }
 }
 
-# Bucket Config
-AWS_STORAGE_BUCKET_NAME = "dept-bucket"
-AWS_S3_REGION_NAME = "eu-west-2"
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-AWS_DOWNLOAD_EXPIRE = 5000
+if "USE_AWS" in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = "dept-bucket"
+    AWS_S3_REGION_NAME = "eu-west-2"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_DOWNLOAD_EXPIRE = 5000
 
-# Static and Media Files
-STATICFILES_STORAGE = "custom_storages.StaticStorage"
-STATICFILES_LOCATION = "static"
+    # Set Signature Version to access files in Bucket
+    AWS_S3_SIGNATURE_VERSION = os.environ.get("AWS_S3_SIGNATURE_VERSION")
 
-MEDIAFILES_STORAGE = "custom_storages.MediaStorage"
-MEDIAFILES_LOCATION = "media"
 
-# Override Static and Media File URLS in production
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
+    # Static and Media Files
+    STATICFILES_STORAGE = "custom_storages.StaticStorage"
+    STATICFILES_LOCATION = "static"
+
+    MEDIAFILES_STORAGE = "custom_storages.MediaStorage"
+    MEDIAFILES_LOCATION = "media"
+
+    # Override Static and Media File URLS in production
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
