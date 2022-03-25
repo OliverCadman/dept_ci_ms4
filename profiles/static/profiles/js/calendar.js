5ccea4e48cf76ae2a966adde5ce1e4e2dc6343cf
Oliver Cadman <o.cadman@live.co.uk>
2022-03-25 12:35:35 +0000

    Avoid duplicates in dateArray, and add comments

--- profiles/static/profiles/js/calendar.js
+++ profiles/static/profiles/js/calendar.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-
-
+  /* Initialize Global Variables */
   let calendar;
-  let dateArray = [];
+
+  // UserId and csrf token to use in POST request
   const userId = $("#user_id_3").val();
   const csrfToken = $("input[name='csrfmiddlewaretoken']")[2].value;
 
@@ -15,6 +15,11 @@ $(document).ready(function() {
     attributes: true,
   };
 
+
+  // Check for "hidden" className change on calendar container.
+
+  /* When fired, an AJAX call is made to the backend to get user's 
+     unavailable dates. */
   const observer = new MutationObserver(callback);
   observer.observe(calendarContainer, options);
   function callback(mutationList, observer) {
@@ -23,7 +28,6 @@ $(document).ready(function() {
         mutation.type === "attributes" &&
         mutation.attributeName === "class"
       ) {
-        const userId = $("#user_id_3").val();
         $.ajax({
           url: `/profile/get_users_unavailable_dates/${userId}`,
           type: "get",
@@ -52,10 +56,15 @@ $(document).ready(function() {
     });
   }
 
-  function initCalendar(existingUnavailableDates) {
+// ------------------------------------------------------
 
-    let eventArray = []
-    let eventId = 1
+  function initCalendar(existingUnavailableDates) {
+    /* Initialize the FullCalendar, and populate with
+    any existing dates returned from the backend, if any.
+    */
+
+    let eventArray = [];
+    let eventId = 1;
     if (existingUnavailableDates) {
       for (date of existingUnavailableDates) {
         let eventObject = {
@@ -63,130 +72,90 @@ $(document).ready(function() {
           start: date,
           allDay: true,
           display: "background",
-          backgroundColor: "#ee9ea2"
-        }
-        eventArray.push(eventObject)
+          backgroundColor: "#ee9ea2",
+        };
+        eventArray.push(eventObject);
       }
     }
 
+    // Create the Calendar with initialView, toolbar styling, and events
     const calendarElement = document.getElementById("calendar_wrapper");
     calendar = new FullCalendar.Calendar(calendarElement, {
       initialView: "dayGridMonth",
-      selectable: true,
       height: 450,
       headerToolbar: {
         left: "title",
         center: "",
-        right: "prev,next"
+        right: "prev,next",
       },
       events: eventArray,
-      dateClick: function (info) {        
-        const events = calendar.getEvents();
-        let eventToRemove;
-        for (let event of events) {  
-          let eventDate = event.start.toISOString().split("T")[0];
-          if (eventDate === info.dateStr) {
-            eventToRemove = event;
-          } 
-        }
-        if (eventToRemove) {
-          console.log(eventToRemove.start.toISOString().split("T")[0]);
-           $.post({
-             url: `/profile/upload_unavailability/${userId}`,
-             data: {
-               event_to_remove: eventToRemove.start.toISOString().split("T")[0],
-               request: 2,
-             },
-             headers: {
-               "X-CSRFToken": csrfToken,
-             },
-             success: function (res) {
-               console.log(res);
-               eventToRemove.remove();
-             },
-           });
-        } else {
-          createEvent(info.dateStr)
-
-        }
-      },
     });
     calendar.render();
+
+    // Event handler for dateClick, call function logDate()
+    calendar.on("dateClick", logDate, { passive: true });
+
+    function logDate(info) {
+      /* 
+        Event handler to log clicked dates.
+
+        If the date-string of the clicked date
+        matches a date already in the collection
+        of FullCalendar's events (populated with
+        AJAX 'get_users_unavailable_dates'), then 
+        make an AJAX POST request to remove the date
+        from the calendar.
+
+        Otherwise, create a calendar event from the clicked
+        date.
+        */
+      const events = calendar.getEvents();
+      let eventToRemove;
+      for (let event of events) {
+        let eventDate = event.start.toISOString().split("T")[0];
+        if (eventDate === info.dateStr) {
+          eventToRemove = event;
+        }
+      }
+
+      // If there is an event to remove, make post request to remove.
+      if (eventToRemove) {
+        $.post({
+          url: `/profile/upload_unavailability/${userId}`,
+          data: {
+            event_to_remove: eventToRemove.start.toISOString().split("T")[0],
+            request: 2,
+          },
+          headers: {
+            "X-CSRFToken": csrfToken,
+          },
+          success: function (res) {
+            eventToRemove.remove();
+          },
+        });
+      } else {
+        // Otherwise, create an event
+        createEvent(info.dateStr);
+      }
+    }
   }
 
   function createEvent(startDate) {
+    /* 
+      Creates a FullCalendar event, with
+      id, startDate and styling. 
+      */
     const event = {
       id: uuidv4(),
       date: startDate,
       display: "background",
-      backgroundColor: "#ee9ea2"
+      backgroundColor: "#ee9ea2",
     };
     calendar.addEvent(event);
-  
   }
-
-  function collectDateArray() {
-    let dates = calendar.getEvents();
-    let ISOStringFormattedDateArray = [];
-
-    for (let i = 0; i < dates.length; i++) {
-      let rawDateString = dates[i]._instance.range.start
-        .toISOString()
-        .split("T")[0];
-      ISOStringFormattedDateArray.push(rawDateString);
-    }
-    
-    return ISOStringFormattedDateArray;
-  }
-
-  const submitBtn = $("#submit_unavailability");
-  let isSubmitting = false;
-
-  submitBtn.on("click", function () {
-    let dateArray = collectDateArray();
-    console.log(csrfToken);
-    if (isSubmitting) {
-      return;
-    }
-    isSubmitting = true;
-    const url = `/profile/upload_unavailability/${userId}`;
-    $.post({
-      url: url,
-      headers: {
-        "X-CSRFToken": csrfToken,
-      },
-      data: {
-        date_array: dateArray,
-      },
-      success: function (res) {
-        isSubmitting = false;
-
-        $("#third_breadcrumb_sm").addClass("fill-breadcrumb-sm");
-        $("#third_breadcrumb_icon").addClass("fill-breadcrumb-icon-sm");
-        $("#third_breadcrumb").addClass("fill-breadcrumb");
-        $("#third_breadcrumb_text").addClass("dark-text");
-
-        Toastify({
-          text: res.success_msg,
-          duration: 10000,
-          close: true,
-          gravity: "top",
-          position: "right",
-          style: {
-            background: "#202020",
-            fontFamily: "'Josefin Sans', sans-serif",
-            color: "#fefefe",
-          },
-        }).showToast();
-        setTimeout(() => {
-          window.location.href = res.url;
-        }, 2500);
-      },
-    });
-    return false;
-  });
 
   function uuidv4() {
+    /* Creates a unique identifier for a created FullCalendar Event  */
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
       (
         c ^
@@ -194,4 +163,75 @@ $(document).ready(function() {
       ).toString(16)
     );
   }
+
+   function collectDateArray() {
+        /*
+          Collects dates added to FullCalendar's
+          collection of added events, and prepares
+          them into the correct format, to be posted
+          to the backend.
+        */
+        let dates = calendar.getEvents();
+        let ISOStringFormattedDateArray = [];
+
+        for (let i = 0; i < dates.length; i++) {
+            let rawDateString = dates[i]._instance.range.start
+            .toISOString()
+            .split("T")[0];
+            ISOStringFormattedDateArray.push(rawDateString);
+         }
+
+        return ISOStringFormattedDateArray;
+   }
+
+  // ---------- Submit Unavailable Dates ---------- //
+  const submitBtn = $("#submit_unavailability");
+  let isSubmitting = false;
+
+  submitBtn.on("click", function () {
+      let dateArray = collectDateArray();
+
+      // Removes any duplicate dates from dateArray
+      dateArray = [...new Set(dateArray)];
+
+      // Avoids duplicate post requests
+      if (isSubmitting) {
+          return;
+        }
+      isSubmitting = true;
+      const url = `/profile/upload_unavailability/${userId}`;
+
+      // Post to /profile/upload_unavailability URL
+      $.post({
+          url: url,
+          headers: {
+              "X-CSRFToken": csrfToken,
+          },
+          data: {
+            date_array: dateArray,
+          },
+          success: function (res) {
+              isSubmitting = false;
+
+              /* Fill third breadcrumb upon successful submission
+              and display toast. Then redirect.
+              */
+              $("#third_breadcrumb_sm").addClass("fill-breadcrumb-sm");
+              $("#third_breadcrumb_icon").addClass("fill-breadcrumb-icon-sm");
+              $("#third_breadcrumb").addClass("fill-breadcrumb");
+              $("#third_breadcrumb_text").addClass("dark-text");
+
+              const successMsg = "You updated your calendar.";
+              displayToast(successMsg, "#287e28");
+
+              setTimeout(() => {
+                  window.location.href = res.url;
+              }, 2500);
+          },
+          error: function (err) {
+              console.log(err);
+          },
+       });
+       return false;
+  });
 })
