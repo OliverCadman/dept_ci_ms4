b2e6a4d1481c68266aac92b3d413442b4ebfdbf2
Oliver Cadman <o.cadman@live.co.uk>
2022-03-25 11:07:25 +0000

    Centered toasts and added comments

--- static/js/toastify.js
+++ static/js/toastify.js
@@ -1,16 +1,30 @@
+    /* 
+    Displays messages provided by Django or from AJAX success callbacks in:
+    - /profiles/static/profiles/js/audio_dropzone.js 
+    - /profiles/static/profiles/js/calendar.js.
+
+    Takes two arguments:
+
+    1. msg - The message being passed.
+    2. representativeColor - A color to represent that nature of the message.
+
+    Utilises the Toastify.js library
+
+    https://apvarun.github.io/toastify-js/
+    */
+    
     function displayToast(message, representativeColor) {
         Toastify({
           text: message,
-          duration: -1,
+          duration: 5000,
           close: true,
           gravity: "top",
-          position: "right",
+          position: "center",
           style: {
             background: "#fefefe",
             fontFamily: "'Josefin Sans', sans-serif",
             color: representativeColor,
-            fontSize: "20px",
-            maxWidth: "100%"
+            fontSize: "18px",
           },
         }).showToast();
       }
