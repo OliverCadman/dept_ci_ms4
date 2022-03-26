4f16c3255265fdcacaff740c8f531998dfaa11a1
Oliver Cadman <o.cadman@live.co.uk>
2022-03-26 17:42:18 +0000

    Added url to view 'remove_offer'

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (DepListView, JobListView, delete_job, post_job, register_interest,
-                    get_interested_members, confirm_job_offer, EditJobView, delete_job)
+                    get_interested_members, confirm_job_offer, EditJobView, delete_job, remove_offer)
 
 urlpatterns = [
     path('find_a_dep', DepListView.as_view(), name="dep_list" ),
@@ -9,6 +9,8 @@ urlpatterns = [
     path("edit_job/<int:job_id>", EditJobView.as_view(), name="edit_job"),
     path("delete_job/<int:job_id>", delete_job, name="delete_job"),
     path("register_interest/<int:job_id>/<str:username>", register_interest, name="register_interest"),
-    path("get_interested_members/<int:job_id>", get_interested_members),
-    path("confirm_job_offer/<int:job_id>/<str:confirmed_user_username>", confirm_job_offer),
+    path("remove_offer/<int:job_id>/<str:username>", remove_offer, name="remove_offer"),
+    path("get_interested_members/<int:job_id>", get_interested_members, name="get_interested_members"),
+    path("confirm_job_offer/<int:job_id>/<str:confirmed_user_username>",
+         confirm_job_offer, name="confirm_job_offer")
 ]
