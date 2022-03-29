e330d71c950e4fa85481ee5600c78edc977c52df
Oliver Cadman <o.cadman@live.co.uk>
2022-03-29 20:07:59 +0100

    Added comments

--- custom_storages.py
+++ custom_storages.py
@@ -1,9 +1,20 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
+"""
+Custom Storages to store Media and Static
+Files using Amazon AWS, in production.
+"""
+
 class StaticStorage(S3Boto3Storage):
+    """
+    Define location for static files.
+    """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """
+    Define location for media files.
+    """
     location = settings.MEDIAFILES_LOCATION
