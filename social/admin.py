050c1e38d3cd225d0250b2e549a84491ff0c4e7d
Oliver Cadman <o.cadman@live.co.uk>
2022-03-29 20:11:36 +0100

    No changes, pushing for testing

--- social/admin.py
+++ social/admin.py
@@ -5,12 +5,12 @@ from .models import Message, Notification
 Admin config for the Social App
 """
 
+
 class MessageAdmin(admin.ModelAdmin):
     """
     Customize list display of message objects
     """
     list_display = ("__str__", "date_of_message",)
-
 
 admin.site.register(Message, MessageAdmin)
 admin.site.register(Notification)
