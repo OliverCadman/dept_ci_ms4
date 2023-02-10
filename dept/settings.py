60c5c9c09dd7cc2d263252d5bad669418a244ec2
Oliver Cadman <79907183+OliverCadman@users.noreply.github.com>
2023-02-10 10:46:42 +0000

    Update settings.py
    Push hack

--- dept/settings.py
+++ dept/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
+
+Some kind of change for triggering deployment...
 """
 
 from pathlib import Path
